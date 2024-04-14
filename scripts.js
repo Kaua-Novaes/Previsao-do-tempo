4834b1aa33e2422974c955e5e48be7ab5e2ff586
Kaua Novaes <kaua88novaes@gmail.com>
2024-04-14 04:56:58 -0300

    modificacoes html e css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-//let key = eef750d8acf37a3d4c6515dd8e783d25
+//seleção de eventos
+const key = 'eef750d8acf37a3d4c6515dd8e783d25';
+const flagapi = "https://flagsapi.com/BR/flat/64.png";
+
+const cidade = document.querySelector("#city-input")
+const buscar = document.querySelector("#pesquisar")
+
+//funções
+buscar.addEventListener("click", (e) =>{
+    e.preventDefault()
+    const city = cidade.value;
+    console.log("eu amo o corinthians")   
+})
+
+//função esperar a cidade
+const previsao = (city) => {
+    console.log("gay")
+}
+
