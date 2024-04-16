71f72824c506597fe9e8d27b8af0dbdd7ae6abea
Kaua Novaes <kaua88novaes@gmail.com>
2024-04-15 23:49:53 -0300

    tratamento de erro

--- scripts.js
+++ scripts.js
@@ -13,7 +13,10 @@ const wheaterElement = document.querySelector("#weather-icon")
 const countryElement = document.querySelector("#country")
 const umidityElement = document.querySelector("#umidity span")
 const windElement = document.querySelector("#wind span")
-
+const telaprevisao  = document.querySelector(".previsao")
+const erro = document.querySelector(".und")
+const paises = document.querySelector(".paises")
+const temp = document.querySelector(".temppais")
 const ConsultarDados = async(city) => {
     const UrlApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${"eef750d8acf37a3d4c6515dd8e783d25"}&lang=pt_br`
     const res = await fetch(UrlApi)
@@ -28,11 +31,24 @@ const ConsultarDados = async(city) => {
 const previsao = async (city) => {
     ConsultarDados(city)
     const data = await ConsultarDados(city);
+
     cityElement.innerText= data.name
+    if(cityElement.textContent == "undefined"){
+        telaprevisao.style.display = "none"
+        erro.style.display = "block"
+        console.log(cityElement)
+    }else{
+        telaprevisao.style.display = "block"
+        erro.style.display = "none"
+    }
     tempElement.innerText = parseInt(data.main.temp)
     wheaterElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
     countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`)
+    windElement.innerText = `${data.wind.speed}km/h`
+    umidityElement.innerText = `${data.main.humidity}%`
     console.log(`https://flagsapi.com/${data.sys.country}/flat/64.png`)
+
+    
     
 
 }
@@ -42,5 +58,25 @@ buscar.addEventListener("click", (e) =>{
     e.preventDefault()
     const city = cidade.value;
     previsao(city)   
+
+
+
 })
+
+cidade.addEventListener("keyup", (e) => {
+    if(e.code == 'Enter'){
+        const city = e.target.value;
+        previsao(city) 
+    }
+})
+
+const principais = async (pais, temp) => {
+    ConsultarDados(pais)
+    const data = await ConsultarDados(city);
+
+    return temp.innerText = parseInt(data.main.temp)
+
+}
+principais(paises, temp)
+
 
